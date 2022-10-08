70f1786d20fdfce552a82f9e2ff395f22cd651b2
Reuben Reny <91770238+ReubenReny03@users.noreply.github.com>
2022-10-08 21:21:03 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,9 +1,22 @@
 from typing import List
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
-
-
+  new_arr_1 = nums1[0:m]
+  new_arr_2 = nums2[0:n]
+  sorted_array = []
+  if m>n:
+    for x in range(len(new_arr_1)):
+      if new_arr_1[x] < new_arr_2[x]:
+        sorted_array.append(new_arr_1[x])
+      else:
+        sorted_array.append(new_arr_2[x])
+  elif m>n:
+    for x in range(len(new_arr_1)):
+      if new_arr_1[x] < new_arr_2[x]:
+        sorted_array.append(new_arr_1[x])
+      else:
+        sorted_array.append(new_arr_2[x])
+       
 # Do not change the following code
 nums1 = []
 nums2 = []
